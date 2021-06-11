1c237cb171951583dfa1a637637d3148b9cbcf8e
Nayeli Huerta <nayeli.14.nh@gmail.com>
2021-06-10 23:38:04 -0400

    Agregar sistemas de rutas

---
+++ web/zgames/resources/views/agregar_juego.blade.php
@@ -0,0 +1,4 @@
+@extends("layouts.master")
+@section("contenido")
+    <h1>Agregar juego</h1>
+@endsection
---
+++ web/zgames/resources/views/ver_consolas.blade.php
@@ -0,0 +1,4 @@
+@extends("layouts.master")
+@section("contenido")
+    <h1>Ver consolas</h1>
+@endsection
